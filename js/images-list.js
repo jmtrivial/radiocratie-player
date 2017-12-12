16af4ff877faec93d740ec550d41125eb13a8dfd
Jean-Marie Favreau <jm.trivial@gmail.com>
2017-12-12 09:30:22 +0100

    Ajout du nom des photos

--- js/images-list.js
+++ js/images-list.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		$("#liste-images").append("<h2>Émission « " + periode + " »</h2>");
 		for(var image in images[periode]) {
 			img = images[periode][image];
-			$("#liste-images").append("<div class=\"img\"><a href=\"" + img + "\"><img src=\"" + img + "\" /></a></div>");
+			$("#liste-images").append("<div class=\"img\"><a style=\"float: left\" href=\"" + img + "\"><img src=\"" + img + "\" /></a> <span style=\"float: left; clear: both\">"+ img + "</span></div>");
 		}
 		
 	}
