2a362e5f9bddda4f2aa3aadda1c27bf888da98de
Jean-Marie Favreau <jeanmarie.favreau@free.fr>
2020-09-01 21:05:16 +0200

    Correct a bug when the next show does not exists

--- js/rc-dynamic.js
+++ js/rc-dynamic.js
@@ -147,10 +147,11 @@ function loadInfos() {
 		url: 'https://admin.radiocratie.com/api/live-info?type=show_content',
 		dataType: 'jsonp',
 		success: function(data){
+			console.log(JSON.stringify(data));
 			currenttime = Date.parse(data.schedulerTime);
 			var shift = data.timezoneOffset * 1000;
 			nexttracktime = Date.parse(data.next.starts) + shift; 
-			if (data.nextShow)
+			if (data.nextShow && data.nextShow.length > 0)
 				nextshowtime = Date.parse(data.nextShow[0].starts);
 
 
